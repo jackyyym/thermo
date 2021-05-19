600c164938fad8f9b9d40f06292fa85c540c46cf
jackyyym <jac.smcgrath@gmail.com>
2021-05-18 20:59:50 -0400

    implemented permissions system. set member role and toggle users as managers.

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 D<span>iscord.py<span> bot created to handle movie night polls. Stores submitted movies in a data.json file.
 
 # Commands
-Movie Night:
+## Movie Night
+*Member Commands:*
 - **submit** \<submission> - *Submit your choice for the movie poll.*
 - **unsubmit** - *Remove your submission from the poll.*
-- **submissions** - *List current movie submissions*
-- **createpoll** - *Create a poll from submitted movies*
-- **newpoll** - *Clear current submissions to begin a new poll*
+- **submissions** - *List current movie submissions.*
+- **createpoll** - *Create a poll from submitted movies.*
+
+*Manager Commands:*
+- **newpoll** - *Clear current submissions to begin a new poll.*
+- **setrole** - *Set the movie night member role.*
+- **togglemanager** \<member> - *Toggle manager permissions for a user.*
 
 Misc:
-- **ping** - *Display current ping*
-- **help** - *Display list of commands*
-- **help** \<command> - *Get info about a particular command*
+- **ping** - *Display current ping.*
+- **help** - *Display list of commands.*
+- **help** \<command> - *Get info about a particular command.*
--- main.py
+++ main.py
@@ -41,11 +41,41 @@ async def on_ready():
 # cog for movie poll commands
 class MovieNight(commands.Cog, name="Movie Night"):
 
+	# check if user is a movie night manager or admin
+	async def is_manager(ctx):
+
+		data = readData(ctx.guild.id)
+		if ctx.author.id not in data["config"]["managers"] and not ctx.author.guild_permissions.administrator:
+			await ctx.send("Only managers and admins can use this command.")
+			return False
+		else:
+			return True
+
+	# check if user has member role
+	async def is_member(ctx):
+		
+		data = readData(ctx.guild.id)
+
+		# check if movie role is set
+		if "movierole" not in data["config"]:
+			await ctx.send("There is no movie night role! create one with `!setrole <role name>`")
+			return False
+		role = ctx.guild.get_role(data["config"]["movierole"])
+
+		# check if user has the role
+		if role not in ctx.author.roles:
+			await ctx.send(f"You don't have the role `{role.name}`!")
+			return False
+		else:
+			return True
+		
+			
 	# submit choice for movie poll
 	@commands.command(	
 		help = "Submit your choice for the movie poll. Recommended format: Movie-Title (Year)",
 		brief = "Submit your choice for the movie poll."
 	)
+	@commands.check(is_member)
 	async def submit(self, ctx, *, submission):
 
 		# load guild json
@@ -69,6 +99,7 @@ class MovieNight(commands.Cog, name="Movie Night"):
 		help = "Remove your submission from the poll.",
 		brief = "Remove your submission from the poll."
 	)
+	@commands.check(is_member)
 	async def unsubmit(self, ctx):
 
 		# load guild json
@@ -92,10 +123,11 @@ class MovieNight(commands.Cog, name="Movie Night"):
 		writeData(ctx.guild.id, data)
 
 	# view list of current movie submissions
-	@commands.command(	
+	@commands.command(
 		help = "Display a list of all current movie submissions. Does not create a poll.",
 		brief = "List current movie submissions"
 	)
+	@commands.check(is_member)
 	async def submissions(self, ctx):
 
 		# load guild json
@@ -118,6 +150,7 @@ class MovieNight(commands.Cog, name="Movie Night"):
 		help = "Create a poll from user-submitted movies. Does not delete submissions.",
 		brief = "Create a poll from submitted movies"
 	)
+	@commands.check(is_manager)
 	async def createpoll(self, ctx):
 
 		# load guild json
@@ -155,11 +188,11 @@ class MovieNight(commands.Cog, name="Movie Night"):
 		await message.edit(content='', embed=embed)
 
 	# deletes previous submissions to start new poll
-	# TODO: configure who can use this command
 	@commands.command(	
 		help = "Clear current submissions to begin a new poll. WARNING: deleted submissions are non-recoverable.",
 		brief = "Clear current submissions to begin a new poll"
 	)
+	@commands.check(is_manager)
 	async def newpoll(self, ctx):
 
 		# load guild json
@@ -170,6 +203,54 @@ class MovieNight(commands.Cog, name="Movie Night"):
 		writeData(ctx.guild.id, data)
 
 		await ctx.send("Ready to recieve submissions for a new poll! Previous submissions have been deleted.")
+	
+	# set the movie night member role, creates it if it doesnt exist
+	@commands.command(
+		help = "Set the movie night member role. Creates the role if it does not already exist.",
+		brief = "Set the movie night member role."
+	)
+	async def setrole(self, ctx, *, rolename):
+
+		# load guild json
+		data = readData(ctx.guild.id)
+
+		# lookup and see if role already exists, create if not
+		role = discord.utils.get(ctx.guild.roles, name=rolename)
+		if role == None:
+			try:
+				role = await ctx.guild.create_role(name=rolename, mentionable=True)
+			except discord.Forbidden:
+				await ctx.send("I don't have the `Manage Roles` permission!")
+				return
+
+		data["config"]["movierole"] = role.id
+		writeData(ctx.guild.id, data)
+
+		await ctx.send(f"`{role.name}` is now the movie night role!")
+
+	# toggle manager permissions for a user
+	@commands.command(	
+		help = "Toggle manager permissions for a user.",
+		brief = "Toggle manager permissions for a user."
+	)
+	@commands.check(is_manager)
+	async def togglemanager(self, ctx, member: discord.Member):
+
+		# load guild json
+		data = readData(ctx.guild.id)
+
+		# currently not a manager
+		if member.id not in data["config"]["managers"]:
+			data["config"]["managers"].append(member.id)
+			await ctx.send(f"`{member.name}` is now a manager!")
+
+		# already a manager
+		else:
+			data["config"]["managers"].remove(member.id)
+			await ctx.send(f"`{member.name}` is no longer a manager!")
+
+		writeData(ctx.guild.id, data)
+
 
 # MISC COMMANDS
 @bot.command(
@@ -179,19 +260,19 @@ class MovieNight(commands.Cog, name="Movie Night"):
 async def ping(ctx):
 	await ctx.send(f"pong! {round(bot.latency * 1000)}ms")
 
-
 # helper functions to read and write to guild json
 def readData(id):
 	try:
 		with open(f"./data/{id}.json", "r+") as f:
 			data = json.load(f)
 	except IOError:
-		data = {"config": {},"submissions": []}
+		data = {"config": {"managers":[]},"submissions": []}
 	return data
 
 def writeData(id, data):
 	with open(f"./data/{id}.json", "w") as f:
 		json.dump(data, f, indent=4)
+
 
 # load cogs
 bot.add_cog(MovieNight(bot))
